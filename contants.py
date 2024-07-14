1cbc8aa2825cf87e458c25c37db1f53149492e80
Oogway <mohammedyousef.dev@gmail.com>
2024-07-14 21:19:46 +0500

    Update translations

--- contants.py
+++
@@ -1,8 +0,0 @@
-BOT_TOKEN = "7044079410:AAGMmE7gAI8pphRbS5qikaAt6LRM3JkrYBc"
-OPENAI_API_KEY = "sk-proj-dEvykWtmPIsgymVv01UUT3BlbkFJB1xxE7VomwGN6YhDvc5V"
-
-LANGUAGES = {
-    'ar': 'Arabic',
-    'uz': 'Uzbek',
-    'ru': 'Russian',
-}

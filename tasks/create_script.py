c37c3196b3f38027d7b6ab4a46c5ec19ebf1a31f
Oogway <mohammedyousef.dev@gmail.com>
2024-07-19 09:00:00 +0500

    Update lotin to krill

--- tasks/create_script.py
+++ tasks/create_script.py
@@ -2,12 +2,23 @@ from celery import shared_task
 from utils.translator import translate_text
 from data.models import Script
 from keyboards import replies
+import requests
 
 
 @shared_task
 def translate_task(text: str, target_language: str, chat_id: int, script_id) -> None:
     from dispatcher import bot
     script = Script.objects.get(id=script_id)
+
+    if target_language == "uz":
+        url = f'https://mal1kov.uz/lotin-kirill-api/latin'
+        headers = {'Content-Type': 'application/json'}
+        data = {'text': text}
+
+        response = requests.post(url, json=data, headers=headers)
+
+        if response.status_code == 200:
+            text = response.json()['result']
 
     result = translate_text(text, target_language)
     texts = result
